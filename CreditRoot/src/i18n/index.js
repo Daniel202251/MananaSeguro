22abf938739924d5c7d8250fad705f8daefa8168
JACOB STANLEY <jaynomjacobstanley@gmail.com>
2026-05-29 16:28:34 +0100

    https://github.com/jaynomyaro/MananaSeguro.git

--- CreditRoot/src/i18n/index.js
+++ CreditRoot/src/i18n/index.js
@@ -3,13 +3,37 @@ import { initReactI18next } from 'react-i18next'
 import es from './es.json'
 import en from './en.json'
 
+// Get stored language preference or detect browser language
+const getInitialLanguage = () => {
+  const storedLang = localStorage.getItem('manana-seguro-language')
+  if (storedLang && (storedLang === 'es' || storedLang === 'en')) {
+    return storedLang
+  }
+  
+  // Detect browser language
+  const browserLang = navigator.language || navigator.userLanguage
+  if (browserLang.startsWith('en')) {
+    return 'en'
+  }
+  
+  // Default to Spanish
+  return 'es'
+}
+
+const initialLanguage = getInitialLanguage()
+
 i18n
   .use(initReactI18next)
   .init({
     resources: { es: { translation: es }, en: { translation: en } },
-    lng: 'es',
+    lng: initialLanguage,
     fallbackLng: 'es',
     interpolation: { escapeValue: false },
   })
+
+// Save language preference to localStorage whenever it changes
+i18n.on('languageChanged', (lng) => {
+  localStorage.setItem('manana-seguro-language', lng)
+})
 
 export default i18n
